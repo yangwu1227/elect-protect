68847dbcf5bd727a673dfd47fb19c9c27e180b6b
YangWu1227 <yangwu2020@gmail.com>
2022-07-04 23:52:34 -0400

    feat: add function for ranking multiple selection columns

--- R/utils.R
+++ R/utils.R
@@ -22,3 +22,53 @@ str_format <- function(str) {
 app_sys <- function(...) {
   system.file(..., package = "ElectProtect")
 }
+
+
+
+#' Function for computing ranked DataFrame
+#'
+#' Function to rank multiple-selection columns after grouping by a modeled column.
+#'
+#' @param df A data frame.
+#' @param cols A character vector of multiple selection column names to include in the ranking.
+#' @param model_col A length-one character vector for the modeled column.
+#' @param multiselect_cat A length-one character vector for the "selected" option for each multiple-selection column.
+#'
+#' @return A data frame.
+#'
+#' @keywords internal
+rank_multi_select <- function(df, cols, model_col, multiselect_cat) {
+  frames <- lapply(
+    X = cols,
+    FUN = function(col) {
+      # Get percentages of 'multiselect_cat' (selection multiple selection option) for each group in 'model_col'
+      frame <- df[,
+                  {
+                    group_weight <- sum(weight)
+                    .SD[, .(percent = sum(weight) / group_weight), by = col]
+                  },
+                  by = model_col
+      ][get(col) == multiselect_cat, c(1, 3)]
+
+      # Renmove NA group in 'model_col'
+      frame <- na.omit(frame, cols = model_col)
+      # Rename
+      setnames(x = frame, old = "percent", new = col)
+      frame
+    }
+  )
+
+  # Join based on modeling column
+  joined_frame <- Reduce(f = function(frame_x, frame_y) {
+    merge(frame_x, frame_y, by = model_col)
+  }, x = frames)
+
+  # Transpose
+  out <- transpose(joined_frame, keep.names = "rn")
+  col_names <- as.character(out[1, ])
+  # Set first row as header
+  out <- out[2:.N]
+  setnames(x = out, old = names(out), new = col_names)
+
+  out
+}
